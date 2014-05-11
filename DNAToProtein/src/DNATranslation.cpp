acca8168d8001336e4cb1503c5cafd31729c0ce7
Wakim Jraige <wakim.jraige@gmail.com>
2014-05-11 11:37:54 -0300

    Translation to english

--- DNAToProtein/src/DNATranslation.cpp
+++ DNAToProtein/src/DNATranslation.cpp
@@ -19,12 +19,12 @@ void print_compact_sequence(const char* seq, int size, const int start_index);
 int get_number_of_chunks(const long sequence_size, int& max_chunk_size);
 
 void print_usage(char* program_name) {
-	printf("Uso: %s arquivo_entrada [tipo] [arquivo_saida] [debug] (tipo := completo | compacto)\n", program_name);
-	printf("No tipo completo cada aa eh representado com tres letras (e.g: MET), no modo compacto apenas uma (e.g: M)\n");
+	printf("Usage: %s path_to_file [output_type] [path_to_output_file] [debug] (type := complete | compact)\n", program_name);
+	printf("In complete type each amino acid is shown on output with three letters (e.g: 'MET' for Methionine).\nIn compact type shown just one letter (e.g: 'M' for Methionine)\n");
 }
 
 int main(int argc, char **argv) {
-	char* sequence, *input_path = NULL, *type = "completo";
+	char* sequence, *input_path = NULL, *type = "complete";
 	char output_path[255];
 	bool debug_flag = false, complete = true;
 	const int max_threads = 128;
@@ -66,9 +66,9 @@ int main(int argc, char **argv) {
 	BioInfo::SequenceWriter<char> writer(output_path);
 
 	if(strlen(type) == 8) {
-		if(strncmp("completo", type, 8) == 0) {
+		if(strncmp("complete", type, 8) == 0) {
 			complete = true;
-		} else if(strncmp("compacto", type, 8) == 0) {
+		} else if(strncmp("compact", type, 8) == 0) {
 			complete = false;
 		}
 	}
@@ -82,7 +82,7 @@ int main(int argc, char **argv) {
 	int remainder = sequence_size % 3;
 	
 	if(remainder) {
-		printf("Alerta, a cadeia de entrada nao eh multipla de 3. Serao ignoradas as %d ultimas bases\n", remainder);
+		printf("Alert, the sequence chain is not multiple of 3. The last %d bases will be ignored\n", remainder);
 	}
 	
 	// Calcula quantas vezes devemos ir para a GPU processar a sequencia inteira.
@@ -92,9 +92,9 @@ int main(int argc, char **argv) {
 	long offset = 0, total_processed = 0;
 
 	if(number_of_chunks > 1) {
-		printf("A sequencia sera processada %d vezes em pedacos de %d bytes\n", number_of_chunks, max_chunk_size);
+		printf("The sequence will be processed %d times in chunks of %d bytes\n", number_of_chunks, max_chunk_size);
 	} else {
-		printf("A sequencia tem %d bytes, o maior pedaco que pode ser tratado de uma vez e %d\n", chunk_size, max_chunk_size);
+		printf("The sequence have  %d bytes, the highest chunk that will be processed by GPU is %d\n", chunk_size, max_chunk_size);
 	}
 
 	writer.open();
@@ -116,11 +116,11 @@ int main(int argc, char **argv) {
 		const char* aa_seq = translator->get_aa_seq(aa_seq_size);
 
 		if(! writer.write(aa_seq, aa_seq_size)) {
-			fprintf(stderr, "Erro ao escrever saida em %s\n", output_path);
+			fprintf(stderr, "Error writing the output in %s\n", output_path);
 			break;
 		}
 
-		printf("Total Processado %ld/%ld pb\n", total_processed, sequence_size);
+		printf("Total Processed %ld/%ld pb\n", total_processed, sequence_size);
 	}
 
 	if(! reader.close()) {
@@ -131,11 +131,11 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Arquivo %s gerado\n", output_path);
+	printf("File %s generated\n", output_path);
 
 	if(debug_flag) {
 		float time = real_translator->get_sum_times();
-		printf("Tempo total gasto na GPU: %f ms\n", time);
+		printf("Total time spent in GPU: %f ms\n", time);
 	}
 
 	delete translator;
@@ -144,7 +144,7 @@ int main(int argc, char **argv) {
 
 	double diff_time = ((double)(stop - start)) / CLOCKS_PER_SEC;
 
-	printf("Tempo total gasto na cpu: %lf ms (%lf s)\n", diff_time * 1000.0, diff_time);
+	printf("Total time spent in CPU: %lf ms (%lf s)\n", diff_time * 1000.0, diff_time);
 
 	WAIT
 	return 0;
